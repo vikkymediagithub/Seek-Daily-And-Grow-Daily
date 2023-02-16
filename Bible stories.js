b4804c4c8469c08b3ec746d454dbbfc6a0e9d3fb
vikkymediagithub <okevictor14@gmail.com>
2023-02-15 16:28:15 -0800

    Bible Story part two and pagination via link

--- Bible stories.js
+++
@@ -1,7 +0,0 @@
-$(window).bind("load resize slid.bs.carousel", function() {
-    var imageHeight = $(".active .holder").height();
-    $(".controllers").height( imageHeight );
-    console.log("Slid");
-  });
-
-//   Pagination function
